77b2c6146912eedeef6e1d4fff0f7631c448f87f
suemor <suemor233@outlook.com>
2023-05-12 01:14:43 +0800

    fix: init theme is undefined

--- src/app/@header/page.tsx
+++ src/app/@header/page.tsx
@@ -1,16 +1,22 @@
 'use client'
 
 import { useTheme } from 'next-themes'
+import { useMemo } from 'react'
 import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md'
 
 const Header = () => {
   const { theme, setTheme } = useTheme()
+  const currentTheme = useMemo(
+    () => (theme === 'light' ? 'dark' : 'light'),
+    [theme],
+  )
   return (
     <div
       className="absolute right-2 top-2 cursor-pointer text-2xl"
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      onClick={() => setTheme(currentTheme)}
+      title={currentTheme === 'light' ? '当前亮色模式' : '当前夜间模式'}
     >
-      {theme === 'light' ? <MdOutlineLightMode /> : <MdOutlineDarkMode />}
+      {theme === 'dark' ? <MdOutlineDarkMode /> : <MdOutlineLightMode />}
     </div>
   )
 }
