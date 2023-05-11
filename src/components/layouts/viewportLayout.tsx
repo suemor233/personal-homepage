722c7b589cdd81061e6de53bedd4650d1d6a1733
suemor <suemor233@outlook.com>
2023-05-11 23:49:50 +0800

    chore: reduce font size

--- src/components/layouts/viewportLayout.tsx
+++ src/components/layouts/viewportLayout.tsx
@@ -2,17 +2,19 @@
 
 import { motion } from 'framer-motion'
 import type { FC, PropsWithChildren } from 'react'
+import { useMedia } from 'react-use'
 
 export const ViewportLayout: FC<PropsWithChildren & { title?: string }> = ({
   children,
   title,
 }) => {
+  const isWide = useMedia('(min-width: 768px)', true)
   return (
     <motion.div
       className="mt-14 md:mt-32 w-full"
       initial={{ opacity: 0, y: 40 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.4 }}
+      viewport={{ once: true, amount: isWide ? 0.2 : 0 }}
       transition={{ duration: 0.3, ease: 'easeInOut' }}
     >
       <h3 className="text-4xl">{title}</h3>
